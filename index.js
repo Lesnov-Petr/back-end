05d0f87a2ba3eb0a621ca8c0e6ea3d5bbdabb45d
Lesnov Petr <lesnov,p@gmail.com>
2023-01-28 16:06:53 +0300

    3

--- index.js
+++ index.js
@@ -2,18 +2,19 @@ const express = require("express");
 const morgan = require("morgan");
 require("dotenv").config();
 const app = express();
-const {
-  connectMongoProductsDB,
-} = require("./src/db/Products/connectProductsDB");
-const { porductsRouter } = require("./src/routers/productsRouter");
+const { connectMongo } = require("./db/Products/connectProductsDB");
+const { porductsRouter } = require("./routers/productsRouter");
 const PORT = process.env.PORT;
 
 app.use(express.json());
 app.use(morgan("tiny"));
 app.use("/api/products", porductsRouter);
+app.use("/products", (req, res) => {
+  return res.json({ message: "products" });
+});
 
 const start = async () => {
-  await connectMongoProductsDB();
+  await connectMongo();
   app.listen(PORT, (err) => {
     if (err) {
       console.log(err);
