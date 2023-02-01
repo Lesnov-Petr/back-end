0e2a3fca5e3639d3b63516944fb81662a73579ce
Lesnov Petr <lesnov,p@gmail.com>
2023-02-01 15:03:15 +0300

    Update serviceProducts.js

--- src/services/serviceProducts.js
+++ src/services/serviceProducts.js
@@ -4,10 +4,7 @@ const searchProducts = async (product) => {
   const allProducts = await Products.find({});
   const normolizeQuery = product.title.trim().toLowerCase().split(" "); //example ["гречка", "зеленый"]
   const listProducts = allProducts.filter((product) => {
-    const normolizeTitleProducts = product.title.ru
-      .trim()
-      .toLowerCase()
-      .split(" ");
+    const normolizeTitleProducts = product.title.ru.toLowerCase().split(" ");
     let coincidences = true;
 
     normolizeQuery.forEach(
