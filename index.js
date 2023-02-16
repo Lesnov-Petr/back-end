e7d968ebf2bc01cd1477b10db6f0fd76da28c1ef
Lesnov Petr <lesnov,p@gmail.com>
2023-02-16 23:19:31 +0300

    add Cors politic

--- index.js
+++ index.js
@@ -9,10 +9,13 @@ const { porductsRouter } = require("./src/routers/productsRouter");
 const { authRouter } = require("./src/routers/authRouter");
 
 const PORT = process.env.PORT;
+const corsOptions = {
+  origin: "http://localhost:3000",
+};
 
 app.use(express.json());
 app.use(morgan("tiny"));
-app.use(cors());
+app.use(cors(corsOptions));
 
 app.use("/api/users", authRouter);
 app.use("/api/products", porductsRouter);
