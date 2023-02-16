05f4ba5370fca0cfb2548765d36a428588fe71ac
Lesnov Petr <lesnov,p@gmail.com>
2023-02-16 23:20:20 +0300

    Update index.js

--- index.js
+++ index.js
@@ -9,9 +9,9 @@ const { porductsRouter } = require("./src/routers/productsRouter");
 const { authRouter } = require("./src/routers/authRouter");
 
 const PORT = process.env.PORT;
-const corsOptions = {
-  origin: "http://localhost:3000",
-};
+// const corsOptions = {
+//   origin: "http://localhost:3000",
+// };
 
 app.use(express.json());
 app.use(morgan("tiny"));
